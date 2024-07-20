111f165482b93d973e6681f7af6e81a66d1f788c
stringerm <stringer.m09@gmail.com>
2024-07-19 23:06:41 -0500

    Second Commit

--- server.R
+++ server.R
@@ -3,27 +3,59 @@ library(dplyr)
 library(plotly)
 library(shiny)
 dat <- read.csv("replication.csv")
+boxdat <- dat
 
 # Define server logic required to draw a histogram
 function(input, output) {
 
     output$boxPlot <- renderPlotly({
-      dat %>%
+      if(input$boxregime != "All") {
+        boxdat <- boxdat[boxdat$v2x_regime == input$boxregime,]
+      }
+      boxdat %>%
         filter(year == input$boxyear) %>%
         plot_ly(x = ~e_regionpol_6C, 
                 y = ~trip_score, type="box", 
-                color = ~e_regionpol_6C,
-                boxpoints = 'all',
-                jitter = 0.6,
-                pointpos = -2
+                color = ~e_regionpol_6C
                 ) %>%
         layout(title = "TRIP Score by Region",
                xaxis = list(title="Region"),
                yaxis = list(title="TRIP Score", 
                             range = c(0,13)),
-               showlegend = FALSE,
-               boxgap = 0.5) 
+               showlegend = FALSE) 
       
+    })
+    output$globalLine <- renderPlotly({
+      linedat <- dat %>%
+        filter(year > 2006) %>%
+        group_by(year,v2x_regime) %>%
+        summarise(avg_trip = mean(trip_score, na.rm = TRUE))
+      
+      trace1 <- linedat %>% filter(v2x_regime == "Liberal Democracy") %>%
+        select(avg_trip)
+      trace2 <- linedat %>% filter(v2x_regime == "Electoral Democracy") %>%
+        select(avg_trip)
+      trace3 <- linedat %>% filter(v2x_regime == "Electoral Autocracy") %>%
+        select(avg_trip)
+      trace4 <- linedat %>% filter(v2x_regime == "Closed Autocracy") %>%
+        select(avg_trip)
+      years <- 2007:2021
+      
+      chartdf <- data.frame(years,trace1,trace2,trace3,trace4)
+      chartdf %>%
+        plot_ly(x = ~years,
+                      y = ~trace1$avg_trip,
+                      name = 'Liberal Democracy',
+                      type="scatter",
+                      mode="lines") %>%
+        add_trace(y = ~trace2$avg_trip, name = 'Electoral Democracy', mode = 'lines') %>%
+        add_trace(y = ~trace3$avg_trip, name = 'Electoral Autocracy', mode = 'lines') %>%
+        add_trace(y = ~trace4$avg_trip, name = 'Closed Autocracy', mode = 'lines') %>%
+        layout(title = "Average Global TRIP Score",
+               xaxis = list(title="Year",
+                            range = c(2007,2021)),
+               yaxis = list(title="Average TRIP Score", 
+                            range = c(0,7)))
     })
 
 }
--- ui.R
+++ ui.R
@@ -7,26 +7,38 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Global TRIP Scores"),
+    titlePanel("Global TRIP Scores: 2007-2021"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
             sliderInput("boxyear",
                         "Select year:",
-                        min = 2002,
+                        min = 2007,
                         max = 2021,
                         step = 1,
-                        value = 2002,
+                        value = 2007,
                         sep='',
                         ticks=FALSE,
                         animate=animationOptions(interval = 1000,
-                                                 loop = TRUE))
+                                                 loop = TRUE)),
+            selectInput(inputId = "boxregime", 
+                        label="Government type:", 
+                        choices = c("All",
+                                    "Liberal Democracy",
+                                    "Electoral Democracy",
+                                    "Electoral Autocracy",
+                                    "Closed Autocracy"
+                        ), 
+                        selected="All")
+            
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotlyOutput("boxPlot")
+            plotlyOutput("boxPlot"),
+            br(),
+            plotlyOutput("globalLine")
         )
     )
 )
